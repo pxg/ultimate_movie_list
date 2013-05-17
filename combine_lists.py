d66cd1b18a4c65b8817ddd059fa35fe779a82195
Pete Graham <petegraham1@gmail.com>
2013-05-17 14:16:36 +0200

    initial commit

---
+++ combine_lists.py
@@ -0,0 +1,9 @@
+# get the lists (do we have API for each? Does each movie have a unique identifier)
+
+# read the lists into pyton lists
+
+# calculate points for each movie:
+# - get weight for each lists
+# - place_points = invert place?
+# - points = weight * place_points
+# - combine the lists
