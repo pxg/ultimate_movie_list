2f2cc561d5b026403dc2baa643dc634d8e6f3839
Pete Graham <petegraham1@gmail.com>
2013-05-17 16:20:35 +0200

    both lists working

--- combine_lists.py
+++ combine_lists.py
@@ -1,6 +1,48 @@
-# get the lists (do we have API for each? Does each movie have a unique identifier)
+import sys
+from pprint import pprint
 
-# read the lists into pyton lists
+# TODO: have extra function for reading the lines into a list
+def get_imdb_list():
+    """
+    Read the imdb file and return a list
+    """
+    #TODO: read the whole file and just get the line we want
+    list_file = 'imdb.txt'
+    name_column = 26
+    f = open(list_file, 'r')
+    list = []
+    pos = 0
+
+    for line in f:
+        pos += 1
+        words = line.split()
+        film = {'pos': pos, 'score': words[2], 'name': line[name_column:-1]}
+        list.append(film)
+    f.close()
+    return list
+
+def get_bfi_list():
+    """
+    Read the imdb file and return a list
+    """
+    list_file = 'bfi_sight_and_sound_2012.txt'
+    f = open(list_file, 'r')
+    list = []
+    for line in f:
+        words = line.split('    ')
+        film = {'pos': words[0], 'name': words[1][:-1]}
+        list.append(film)
+    f.close()
+    return list
+
+
+imdb_list = get_imdb_list()
+bfi_list = get_bfi_list()
+#calc scores for the bfi_list based of the imdb_list scores
+#bfi_list = calc_scores(bfi_list, imdb_list)
+
+pprint(bfi_list)
+#pprint(imdb_list)
 
 # calculate points for each movie:
 # - get weight for each lists
