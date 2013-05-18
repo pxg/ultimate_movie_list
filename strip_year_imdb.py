74be8bec1afed26b9367c5551fd93b0b713afbcf
Pete Graham <petegraham1@gmail.com>
2013-05-18 16:22:57 +0200

    removing prototypes

--- strip_year_imdb.py
+++
@@ -1,7 +0,0 @@
-name = 'The Artist (2011/I)'
-# could be problematic is there are brackets in the film name
-year = name[name.find('(') + 1:name.find(')')]
-name = name.replace('(' + year + ')', '')
-
-print name
-print year
