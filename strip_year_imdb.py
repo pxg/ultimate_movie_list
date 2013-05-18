b43fee4b1903fee131b58d25bc85bb3e2057c6e7
Pete Graham <petegraham1@gmail.com>
2013-05-18 15:22:38 +0200

    remove unecessary comments

--- strip_year_imdb.py
+++ strip_year_imdb.py
@@ -1,20 +1,7 @@
-import re
-
 name = 'The Artist (2011/I)'
-# match = re.search(r'\([^)]*\)', name)
-# #year_match = re.search('2011', name)
-# # name = re.sub(r'\([^)]*\)', '', name)
-# # print name
-# if match:
-#     print 'match!'
-#     print match.groups()
-# else:
-#     print 'no match'
-
 # could be problematic is there are brackets in the film name
 year = name[name.find('(') + 1:name.find(')')]
 name = name.replace('(' + year + ')', '')
 
-#name = str.replace(name, '(' + year + ')')
 print name
 print year
