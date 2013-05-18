8ab808694013e4f4b8b515c15a81eb4f86893792
Pete Graham <petegraham1@gmail.com>
2013-05-18 20:06:25 +0200

    reworked json api and removed seperate view functions

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ class DecimalEncoder(json.JSONEncoder):
         super(DecimalEncoder, self).default(o)
 
 
+def json_api_response(list):
+    """
+    json output for api
+    """
+    return Response(json.dumps(list, cls=DecimalEncoder),
+                    mimetype='application/json')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -24,70 +32,53 @@ def index():
 @app.route('/ultimate-list')
 def ultimate_list():
     film_list = get_ultimate_movie_list()
-    return render_template('list.html', film_list=film_list)
-
-
-@app.route('/ultimate-list/json')
-def ultimate_list_api():
-    film_list = get_ultimate_movie_list()
-    return Response(json.dumps(film_list, cls=DecimalEncoder),
-                    mimetype='application/json')
+    if request.args.get('format') == 'json':
+        return json_api_response(film_list)
+    else:
+        return render_template('list.html', film_list=film_list)
 
 
 @app.route('/imdb')
 def imdb():
     film_list = get_imdb_list()
-    return render_template('list.html',
-                           film_list=film_list,
-                           title='IMDB Top 250',
-                           url='http://www.imdb.com/chart/top')
-
-
-@app.route('/imdb/json')
-def imdb_api():
-    film_list = get_imdb_list()
-    return Response(json.dumps(film_list, cls=DecimalEncoder),
-                    mimetype='application/json')
+    if request.args.get('format') == 'json':
+        return json_api_response(film_list)
+    else:
+        return render_template('list.html',
+                               film_list=film_list,
+                               title='IMDB Top 250',
+                               url='http://www.imdb.com/chart/top')
 
 
 @app.route('/sight-and-sound')
 def sight_and_sound():
     film_list = get_bfi_list_with_scores(get_imdb_list())
-    return render_template('list.html',
-                           film_list=film_list,
-                           title='BFI top 11 films of 2012',
-                           url='http://www.bfi.org.uk/news-opinion/sight-sound-magazine/polls-surveys/top-11-films-2012')
-
-
-@app.route('/sight-and-sound/json')
-def sight_and_sound_api():
-    film_list = get_bfi_list_with_scores(get_imdb_list())
-    return Response(json.dumps(film_list, cls=DecimalEncoder),
-                    mimetype='application/json')
+    if request.args.get('format') == 'json':
+        return json_api_response(film_list)
+    else:
+        return render_template('list.html',
+                               film_list=film_list,
+                               title='BFI top 11 films of 2012',
+                               url='http://www.bfi.org.uk/news-opinion/sight-sound-magazine/polls-surveys/top-11-films-2012')
 
 
 @app.route('/oscar-best-picture')
 def oscar_best_picture():
     film_list = get_oscars_best_picture_list()
-    return render_template('list.html',
-                           film_list=film_list,
-                           title='Oscar Best Picture',
-                           url='http://www.oscars.org/awards/academyawards/legacy/best-pictures.html')
-
-
-@app.route('/oscar-best-picture/json')
-def oscar_best_picture_api():
-    film_list = get_oscars_best_picture_list()
-    return Response(json.dumps(film_list, cls=DecimalEncoder),
-                    mimetype='application/json')
+    if request.args.get('format') == 'json':
+        return json_api_response(film_list)
+    else:
+        return render_template('list.html',
+                               film_list=film_list,
+                               title='Oscar Best Picture',
+                               url='http://www.oscars.org/awards/academyawards/legacy/best-pictures.html')
 
 
 @app.route('/oscar-all')
 def oscar_all():
     film_list = get_all_oscars_list()
     if request.args.get('format') == 'json':
-        return Response(json.dumps(film_list, cls=DecimalEncoder),
-                        mimetype='application/json')
+        json_api_response(film_list)
     else:
         return render_template('list.html',
                                film_list=film_list,
