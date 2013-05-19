5576a99669351cf4b9a42345fdec8208e86faf0f
Pete Graham <petegraham1@gmail.com>
2013-05-19 13:10:42 +0200

    removing unecessary code

--- models/sa_film.py
+++ models/sa_film.py
@@ -18,15 +18,4 @@ def get_film_class(declarative_base):
         year = Column(String(120), nullable=True)  # TODO: change type
         created = Column(DateTime(), default=datetime.datetime.utcnow)
         modified = Column(DateTime())  # can we autoset this?
-
-        def __getstate__(self):
-            # TODO: do we even need this? Or is it just a user thing?
-            return {
-                'id': self.id,
-                'name': self.name,
-                'director': self.directory,
-                'year': self.year,
-                'created': self.created,
-                'modified': self.modified,
-            }
     return Film
